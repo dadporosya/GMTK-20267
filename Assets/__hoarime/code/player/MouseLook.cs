6f699438849a968b5b77e03e8a21c0836a03e9a9
dadporosya <ilja.amogus@gmail.com>
2026-07-25 00:01:24 +0300

    after hoarime

--- Assets/__hoarime/code/player/MouseLook.cs
+++ Assets/__hoarime/code/player/MouseLook.cs
@@ -19,8 +19,6 @@ public class MouseLook : MonoBehaviour
     [SerializeField] float minX;
     [SerializeField] float maxX;
     [SerializeField] bool limitX;
-    [Tooltip("Clamp vertical look (pitch / xRot) to [minY, maxY]. Off = free pitch.")]
-    [SerializeField] bool limitY;
 
     public bool canLook = true;
     private bool frozen;
@@ -158,10 +156,7 @@ public class MouseLook : MonoBehaviour
 
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
         xRot -= mouseY;
-        if (limitY)
-        {
-            xRot = Mathf.Clamp(xRot, minY, maxY);
-        }
+        xRot = Mathf.Clamp(xRot, minY, maxY);
         cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, Quaternion.Euler(xRot, cam.transform.eulerAngles.y, 0), smoothing * Time.deltaTime);
     }
 
